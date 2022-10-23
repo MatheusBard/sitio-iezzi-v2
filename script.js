8c679aa4a025239c7f93d2c7d2c7a091ec6b34e5
MatheusBard <matheusbard@gmail.com>
2022-10-23 18:18:35 -0300

    animação showcase ok

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 $(document).ready(function () {
   const showcase = document.getElementById('showcase');
   const showcase__txt = document.querySelectorAll('.showcase__txt');
+  const firstTimeShowing = document.querySelectorAll('.first-time-showing');
 
-  showcase__txt[0].classList.add('start-animation');
-  showcase__txt[0].classList.remove('placeholder');
+  setTimeout(() => {
+    showcase__txt[0].classList.add('start-animation');
+  }, '1000');
+  
+  setTimeout(() => {
+    firstTimeShowing.forEach(elem => {
+      elem.classList.remove('first-time-showing');
+    });
+  }, '1500');
 
   showcase.addEventListener('slide.bs.carousel', e => {
-    showcase__txt[e.from].classList.remove('start-animation');
+    setTimeout(() => {
+      showcase__txt[e.to].classList.add('start-animation');
+    }, '200');
   });
 
   showcase.addEventListener('slid.bs.carousel', e => {
-    showcase__txt[e.to].classList.add('start-animation');
+    showcase__txt[e.from].classList.remove('start-animation');
   });
 });
