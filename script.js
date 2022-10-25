e0f307699a8d158c51baa6520d767cc2a82471f3
MatheusBard <matheusbard@gmail.com>
2022-10-24 22:53:31 -0300

    header q 'aparece'

--- script.js
+++ script.js
@@ -1,18 +1,34 @@
 $(document).ready(function () {
+  
+  const slidingHeader = document.getElementById('sliding-header');
   const showcase = document.getElementById('showcase');
   const showcase__txt = document.querySelectorAll('.showcase__txt');
   const firstTimeShowing = document.querySelectorAll('.first-time-showing');
 
+  if(!!window.IntersectionObserver) {
+    const showcaseObserver = new IntersectionObserver (entries => {
+      if(!entries[0].isIntersecting) slidingHeader.classList.add('test')
+      else slidingHeader.classList.remove('test');
+    }, {
+      rootMargin: "-1px"
+    });
+
+    showcaseObserver.observe(showcase);
+  }
+
+  // Animação da primeira PALAVRA ao carregar a página
   setTimeout(() => {
     showcase__txt[0].classList.add('start-animation');
   }, '1000');
   
+  // Animação dos botões do carrossel ao carregar a página
   setTimeout(() => {
     firstTimeShowing.forEach(elem => {
       elem.classList.remove('first-time-showing');
     });
   }, '1500');
 
+  // Animação das PALAVRAS ao mudar de slide
   showcase.addEventListener('slide.bs.carousel', e => {
     setTimeout(() => {
       showcase__txt[e.to].classList.add('start-animation');
